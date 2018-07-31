0dad13e0d037ad3de92a4e5129563e4cc0a94964
周卫东 <zhouweidong@zhangyue.com>
2018-07-31 17:29:03 +0800

    Initial Commit

---
+++ CoreText/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CoreText
+//
+//  Created by 周卫东 on 2018/7/31.
+//  Copyright © 2018年 周卫东. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CoreText/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  CoreText
+//
+//  Created by 周卫东 on 2018/7/31.
+//  Copyright © 2018年 周卫东. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
